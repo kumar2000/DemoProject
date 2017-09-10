42b973e1ec9d14d8767fe69979b14760e8ee4037
Prabhu Nayidi <prabhun.1600@gmail.com>
2017-09-10 11:25:23 -0500

    Revert "Commented some code"
    This reverts commit 2830aaf2ca31472950c09995b40c9a774a399bdb.

--- Java Basics/Module1/src/FirstJavaProgram.java
+++ Java Basics/Module1/src/FirstJavaProgram.java
@@ -20,8 +20,6 @@ public class FirstJavaProgram {
 		System.out.println(b1);
 		System.out.println(b2);
 		
-		/*
-		
 		int a = 1000000000;
 		int b = 0;
 		System.out.println(a);
@@ -46,8 +44,6 @@ public class FirstJavaProgram {
 		
 		boolean e = true;
 		System.out.println(e);
-		
-		*/
 		
 		String s = "I am learning the Java Basics for Selenium Testing Course!";
 		System.out.println(s);
